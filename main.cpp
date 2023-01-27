83eebf9203321dfdf8885af84e88e9aa7dc084c6
peerpressurre <gsophiay@gmail.com>
2023-01-27 17:25:23 +0200

    1f

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
- 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "Enter a number-> ";
+	cin >> num;
+	for (size_t i = 0; i < num + 1; i++)
+	{
+		cout << i << "  ";
+	}
+	return 0;
+}
