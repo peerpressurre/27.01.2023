3d2848613b75d9888c8230528ca68d0f9affc256
peerpressurre <gsophiay@gmail.com>
2023-01-27 17:38:08 +0200

    4f

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
- 
+#include <iostream>
+#include <algorithm>
+#include <list>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "-> ";
+	cin >> num;
+	int sum = num;
+	for (int i = num; i != 0;)
+	{
+		cout << "-> ";
+		cin >> i;
+		sum += i;
+	}
+
+	cout << "Sum = " << sum << endl;
+
+	return 0;
+}
