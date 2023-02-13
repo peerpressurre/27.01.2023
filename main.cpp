e49bfd7e22ea9e053b2da75079928395cad821bb
peerpressurre <gsophiay@gmail.com>
2023-02-13 20:18:15 +0200

    срусл

--- main.cpp
+++ main.cpp
@@ -3,58 +3,57 @@
 #include <iostream>
 #include <iomanip>
 #include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <ctime>
 #include <chrono>
-
 using namespace std;
 using namespace std::chrono;
-
 int main()
 {
-	int u_num, c_num, counter = 0;
-	time_t start, end, total;
-	srand(time(0));
-	c_num = rand() % 10;
-	cout << c_num << endl; 
-	do
-	{
-		if (counter == 0)
-		{
-			start = system_clock::to_time_t(system_clock::now());
-		}
-
-		counter++;
-		cout << counter << "try" << endl;
-		cout << "[between 0 and 500]" << endl;
-		cout << "Your guess-> ";
-		cin >> u_num;
-		if (u_num < 0 || u_num > 500)
-		{
-			cout << "Wrong range, try again" << endl;
-		}
-		else if (u_num != c_num)
-		{
-			(u_num > c_num) ? cout << "Too big" << endl : cout << "Too small" << endl;
-		}
-		else if (u_num == c_num)
-		{
-			system("cls");
-			end = system_clock::to_time_t(system_clock::now());
-
-			tm* s_tm = localtime(&start);
-			tm* e_tm = localtime(&end);
-			tm* t_tm = localtime(&total);
-
-			/*std::chrono::duration<double, std::micro> elapsed_seconds = ((end) - start);*/
-			std::chrono::duration<double, std::milli> elapsed_seconds = end - start;
-			/*auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-			auto elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);*/
-			std::cout << "Elapsed Time: " << elapsed_seconds.count() << " sec" << std::endl;
-
-
-		}
-	} while (true);
-	
-
-	return 0;
+    srand(time(0));
+    unsigned short user_number, rand_number = rand() % 500;
+    unsigned long long try_count = 0;
+    time_t start, end, total;
+    cout << rand_number << endl;
+    do {
+        if (try_count == 0)
+        {
+            //start = time(0); 
+            start = system_clock::to_time_t(system_clock::now());
+        }
+        // convert now to string form 
+        try_count++;
+        cout << "Try: " << try_count << endl;
+        cout << "number[0 - 500]->";
+        cin >> user_number;
+        if (user_number > 500 || user_number < 0)
+        {
+            cout << "Incorrect number!" << endl;
+            try_count--;
+            system("pause");
+        }
+        else if (user_number != rand_number)
+        {
+            (user_number > rand_number) ? cout << "User number more then rand number" << endl : cout << "User number less then rand number";
+        }
+        else if (user_number == rand_number)
+        {
+            system("cls");
+            end = system_clock::to_time_t(system_clock::now());
+            tm* s_tm = localtime(&start);
+            tm* e_tm = localtime(&end);
+            tm* t_tm = localtime(&total);
+            std::chrono::duration<double> elapsed_seconds = end - start;
+            std::cout << "Elapsed Time: " << elapsed_seconds.count() << " sec" << std::endl;
+            cout << "#>--------<WIN>-----------#" << endl;
+            cout << "| Try count =" << try_count << endl;
+            cout << "| Time start =" << ctime(&start) << endl;
+            cout << "| Time end =" << ctime(&end) << endl;
+            cout << "| Total time =" << ctime(&total) << endl;
+            cout << "#>------------------------#" << endl;
+            exit(0);
+        }
+    } while (true);
+    return 0;
 }
