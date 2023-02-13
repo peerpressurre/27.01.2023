63f36caf08fd3d11ed90184247c210ddbf5de3f0
peerpressurre <gsophiay@gmail.com>
2023-02-13 22:13:00 +0200

    1 500 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
 #include <iomanip>
 #include <stdlib.h>
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
 #include <ctime>
 #include <chrono>
+#include "windows.h"
 
 using namespace std;
 using namespace std::chrono;
@@ -14,6 +16,7 @@ int main()
     unsigned short user_number, rand_number = rand() % 500;
     unsigned long long try_count = 0;
     cout << rand_number << endl;
+    cout << "*Press 0 to quit" << endl;
     std::chrono::system_clock::time_point start, end, total;
     do {
         if (try_count == 0)
@@ -24,17 +27,22 @@ int main()
         // convert now to string form 
         try_count++;
         cout << "Try: " << try_count << endl;
-        cout << "number[0 - 500]->";
+        cout << "Guess a number[0 - 500]->";
         cin >> user_number;
-        if (user_number > 500 || user_number < 0)
+        if (user_number == 0)
         {
-            cout << "Incorrect number!" << endl;
+            break;
+        }
+        else if (user_number > 500 || user_number < 0)
+        {
+            cout << "Number out of range! Try again" << endl;
             try_count--;
             system("pause");
         }
         else if (user_number != rand_number)
         {
-            (user_number > rand_number) ? cout << "User number more then rand number" << endl : cout << "User number less then rand number";
+            (user_number > rand_number) ? cout << "Too much" << endl : cout << "Too little" << endl;
+
         }
         else if (user_number == rand_number)
         {
@@ -43,12 +51,10 @@ int main()
 
             auto elapsed_seconds = chrono::duration_cast<chrono::seconds>(end - start).count();
 
-            cout << "Elapsed Time: " << elapsed_seconds << " sec" << std::endl;
+
             cout << "#>--------<WIN>-----------#" << endl;
-            cout << "| Try count = " << try_count << endl;
-            cout << "| Time start = " << std::chrono::system_clock::to_time_t(start) << endl;
-            cout << "| Time end = " << std::chrono::system_clock::to_time_t(end) << endl;
-            cout << "| Total time = " << std::chrono::system_clock::to_time_t(total) << endl;
+            cout << "| Tries taken: " << try_count << endl;
+            cout << "| Time taken: " << elapsed_seconds << " sec" << std::endl;
             cout << "#>------------------------#" << endl;
             exit(0);
         }
