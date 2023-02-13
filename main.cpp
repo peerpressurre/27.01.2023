40146795efa3346c99829b9fc8a6ca392d2b9eda
peerpressurre <gsophiay@gmail.com>
2023-02-13 18:11:36 +0200

    4ff

--- main.cpp
+++ main.cpp
@@ -5,9 +5,6 @@ using namespace std;
 
 int main()
 {
-	/*int num;
-	cout << "-> ";
-	cin >> num;*/
 	int sum = 0;
 	cout << "Enter numbers: " << endl;
 	for (int i = 1; i != 0;)
@@ -16,8 +13,6 @@ int main()
 		cin >> i;
 		sum += i;
 	}
-
 	cout << "Sum = " << sum << endl;
-
 	return 0;
 }
